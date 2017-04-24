85adbd0efd1ddc13670fd6ad9fe2067938f5a88c
Jani Eväkallio <jani.evakallio@gmail.com>
2017-04-24 11:19:23 +0100

    Implement basic draggable TextInput

---
+++ index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { TextInput, View, Dimensions } from 'react-native';
+
+import Draggable from 'react-native-snap-drag';
+
+const window = Dimensions.get('window');
+const size = 30;
+
+export default class EmojiPicker extends React.Component {
+  render() {
+    const { children, isVisible } = this.props;
+    return (
+      <View
+        style={{
+          flex: 1,
+          overflow: 'hidden'
+        }}
+      >
+        {children}
+        {isVisible &&
+          <Draggable
+            xLocked
+            key={'snap-text-picker'}
+            style={{
+              position: 'absolute',
+              top: window.height / 2 - 100,
+              left: 0
+            }}
+          >
+            <TextInput
+              autoFocus
+              style={{
+                textAlign: 'center',
+                alignItems: 'center',
+                backgroundColor: 'rgba(0,0,0,0.75)',
+                color: 'white',
+                height: size,
+                width: window.width,
+                paddingVertical: 5
+              }}
+            />
+          </Draggable>}
+      </View>
+    );
+  }
+}
